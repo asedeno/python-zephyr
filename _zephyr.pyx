9c664fac18968ee83ed3debc6f7614457c084793
Alejandro R. Sedeño <asedeno@mit.edu>
2020-08-02 00:30:42 -0400

    zuid.tv's unsigned ints are in network order
    Update our patch to the version that was upstreamed.
    
    (cherry picked from commit b323421d446466cdbd64bbde2dfcc11b2c99b674)

--- _zephyr.pyx
+++ _zephyr.pyx
@@ -30,9 +30,9 @@ cdef void _ZUid_c2p(ZUnique_Id_t * uid, object p_uid) except *:
 
 cdef void _ZUid_p2c(object uid, ZUnique_Id_t * c_uid) except *:
     inet_aton(uid.address, &c_uid.zuid_addr)
-    c_uid.tv.tv_sec = int(uid.time)
-    c_uid.tv.tv_usec = socket.htonl(int((uid.time - c_uid.tv.tv_sec) * 100000))
-    c_uid.tv.tv_sec = socket.htonl(c_uid.tv.tv_sec)
+    seconds = int(uid.time)
+    c_uid.tv.tv_sec = socket.htonl(seconds)
+    c_uid.tv.tv_usec = socket.htonl(int((uid.time - seconds) * 100000))
 
 cdef char * _string_p2c(object_pool *pool, object string) except *:
     if string is None:
